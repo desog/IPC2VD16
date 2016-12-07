fdce61c238726e3d31106aa6c44a47e2eed2a925
Unknown <201503692@ingenieria.usac.edu.gt>
2016-12-06 20:26:47 -0600

    Ordenamiento Burbuja
     Ordenamiento de 6 numeros en forma Ascendente

--- HT1/HT1/Form1.Designer.cs
+++ HT1/HT1/Form1.Designer.cs
@@ -39,6 +39,14 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -93,10 +101,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(564, 36);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(103, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "MCD";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
@@ -133,24 +142,101 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Ingrese 3 Numeros para calcular el MCD";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Numero 4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(154, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Numero 5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(254, 143);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Numero 6";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(40, 168);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(59, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(157, 168);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(59, 20);
+            this.textBox6.TabIndex = 3;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(257, 168);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(59, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(564, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Ordenamiento";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(43, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Calcular";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(718, 362);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,6 +255,14 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- HT1/HT1/Form1.cs
+++ HT1/HT1/Form1.cs
@@ -28,26 +28,94 @@ namespace HT1
             int contador = 2;
             int mcd = 1;
 
-            while (contador <= n1 && contador <= n2 && contador <= n3) { 
-                while(n1%contador==0 && n2%contador==0 && n3 % contador==0){
-                mcd=mcd*contador;
-                    n1=n1/contador;
-                    n2=n2/contador;
-                    n3=n3/contador;
+            while (contador <= n1 && contador <= n2 && contador <= n3)
+            {
+                while (n1 % contador == 0 && n2 % contador == 0 && n3 % contador == 0)
+                {
+                    mcd = mcd * contador;
+                    n1 = n1 / contador;
+                    n2 = n2 / contador;
+                    n3 = n3 / contador;
                 }
-                contador=contador+1;
+                contador = contador + 1;
             }
-            textBox4.Text=mcd.ToString() ;
+            textBox4.Text = mcd.ToString();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            label5.Text = "Ingrese 6 numeros para Ordenarlos en Forma Ascendente";
-        }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            label5.Text = "Ingrese 6 numeros para Ordenarlos en Forma Ascendente";
+            textBox5.Show();
+            textBox6.Show();
+            textBox7.Show();
+            label6.Show();
+            label7.Show();
+            label8.Show();
+            button4.Show();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
             label5.Text = "Ingrese 3 Numeros para calcular el MCD";
+            textBox5.Hide();
+            textBox6.Hide();
+            textBox7.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            button4.Hide();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox5.Hide();
+            textBox6.Hide();
+            textBox7.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            button4.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = 0,m=0;
+            int[] num;
+            string resultado;
+            resultado = "";
+            num = new int[6];
+
+            num[0] = int.Parse(textBox1.Text);
+            num[1] = int.Parse(textBox2.Text);
+            num[2] = int.Parse(textBox3.Text);
+            num[3] = int.Parse(textBox5.Text);
+            num[4] = int.Parse(textBox6.Text);
+            num[5] = int.Parse(textBox7.Text);
+
+            for (int i = 1; i < 6; i++) {
+                for (int y = 0; y < 6 - 1; y++) {
+                    if (num[i] < num[y]) {
+                        m = num[i];
+                        num[i] = num[y];
+                        num[y] = m;
+                    }
+                }
+            }
+
+            for (int r = 0; r < 6; r++) {
+                resultado = resultado+ Convert.ToString(num[r])+",";
+            }
+
+            textBox4.Text = resultado;
         }
     }
 }
